0af05d8898c3e762d5487f7c1e0ae4c791b7e9a4
marcus817 <marcus817@gmail.com>
2023-11-16 10:49:28 -0300

    Adicionando Produtos

--- main.py
+++ main.py
@@ -1,8 +1,50 @@
 from fastapi import FastAPI
-
+from typing import List, Dict, Any, Optional
+from pydantic import BaseModel
 app = FastAPI()
 
+class Produto(BaseModel):
+    id: int
+    titulo: str
+    descricao: Optional[str]
+    preco: float
+
+produtos: List[Dict[str, Any]] = [
+    {
+        "id":1,
+        "titulo": "Cadeira Gamer",
+        "descricao":  "Cadeira confortável para fazer live",
+        "preço": 5.0
+    },
+    {
+        "id":2,
+        "a titulo": "Workshop",
+        "descricao": "Workshop de Deploy",
+        "preco": 100.0
+    },
+    {
+        "id":3,
+        "a titulo":"Iphone",
+        "descricao":"Iphone 14",
+        "preco": None
+    }
+]
+
+id_atual = 3 
+
+def lista(self):
+    return self.produtos
+
+def inserir(self, item: Dict[str, any]) -> Dict[str, any]:
+    self.id_atual += 1
+    item["id"] = self.id_atual
+    return self.produtos.append(item)
+ 
+@app.get("/produtos")
+def listar_produtos():
+    return produtos
 ##Função Iniical da API 
 @app.get("/")
 def ola_mundo():
     return {"Hello": "World"}
+
